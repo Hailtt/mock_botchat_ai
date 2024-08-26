c35ff2afd67f8b97252470463d834d2b1d92f932
Luong Tien Thuan Hai <hailtt.work@gmail.com>
2024-08-26 22:47:06 +0700

    first commit

---
+++ botchat_mock_api.js
@@ -0,0 +1,57 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = 4000;
+const DATA_RESPONE = require("./botchat_model");
+app.use(express.json());
+
+app.post("/legal-chat/legalchatbot", async (req, res) => {
+  try {
+    const { request_id, question } = req.body;
+
+    //Kiem tra du lieu tu FE
+    if (!request_id || !question) {
+      return res.status(400).json({
+        code: 1,
+        msg: "Thiếu request_id hoặc question",
+        data: {},
+        request_id: request_id || null,
+      });
+    }
+
+    //Kiem tra data tu botchat
+    if (!DATA_RESPONE) {
+      return res.json({
+        code: 2,
+        msg: "Không xử lý",
+        data: {},
+        request_id: request_id,
+      });
+    }
+
+    // get data
+    const responeData = {
+      code: 0,
+      mgs: "Successful",
+      request_id: request_id,
+      data: {
+        ...DATA_RESPONE,
+        answer: `Câu trả lời cho câu hỏi: ${req.body.question} là:`,
+      },
+    };
+
+    // Trả về phản hồi
+    return res.json(responeData);
+  } catch (error) {
+    res.json({
+      code: 1,
+      msg: "something went wrong !",
+      data: {},
+      request_id: req.body.request_id,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`BOTCHAT API is running on http://localhost:${PORT}`);
+});
---
+++ botchat_model.js
@@ -0,0 +1,23 @@
+const DATA_RESPONE = {
+  answer: "Người bệnh là người sử dụng dịch vụ khám bệnh, chữa bệnh.",
+  metadata: {
+    "7e44e9ec-9f5e-4dfa-b3b8-fa3f83a4b723": {
+      source: "Luật khám bệnh, chữa bệnh 2023",
+      serial_number: "15/2023/QH15",
+      ref: "/yuu/workspaces/legalchat/law/15_2023_QH15.pdf",
+      article_no: "2",
+      article_title: "Giải thích từ ngữ",
+      clause_no: "3",
+    },
+    "40cfdf14-fce5-4920-ba74-c932578be0f3": {
+      source: "Luật khám bệnh, chữa bệnh 2023",
+      serial_number: "15/2023/QH15",
+      ref: "/yuu/workspaces/legalchat/law/15_2023_QH15.pdf",
+      article_no: "2",
+      article_title: "Giải thích từ ngữ",
+      clause_no: "11",
+    },
+  },
+};
+
+module.exports = DATA_RESPONE;

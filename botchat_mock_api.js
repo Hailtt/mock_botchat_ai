fe1a57d3a1d000c202ea2b0737290488d7882d0c
Luong Tien Thuan Hai <hailtt.work@gmail.com>
2024-08-29 15:09:16 +0700

    add text

--- botchat_mock_api.js
+++ botchat_mock_api.js
@@ -1,5 +1,4 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
 const PORT = 4000;
 const DATA_RESPONE = require("./botchat_model");
@@ -36,7 +35,14 @@ app.post("/legal-chat/legalchatbot", async (req, res) => {
       request_id: request_id,
       data: {
         ...DATA_RESPONE,
-        answer: `Câu trả lời cho câu hỏi: ${req.body.question} là:`,
+        answer: `Câu trả lời cho câu hỏi: ${req.body.question} là: 
+        - Thu nhập tính thuế (sau đây viết tắt là TNTT) = Thu nhập chịu thuế - Các khoản giảm trừ
+
+        - Thu nhập chịu thuế bằng tổng số tiền lương, tiền công, tiền thù lao, các khoản thu nhập khác có tính chất tiền lương, tiền công trừ (-) Các khoản thu nhập được miễn thuế (đối với thu nhập từ tiền lương, tiền công).
+
+        - Thuế suất: áp dụng theo Biểu thuế lũy tiến từng phần quy định tại khoản 2 Điều 7 Thông tư 111/2013/TT-BTC.
+
+        - Số thuế thu nhập cá nhân được tính theo từng bậc thu nhập bằng thu nhập tính thuế của bậc thu nhập nhân (×) với thuế suất tương ứng của bậc thu nhập đó (hoặc có thể áp dụng theo Biểu tính thuế rút gọn bên dưới).`,
       },
     };
 
